294fd0b244d52b392148488e167f8aadb6b7c878
WoGoLo <wgl373711794@163.com>
2018-12-12 20:32:50 +0800

    最终提交代码

--- admin/js/common.js
+++ admin/js/common.js
@@ -6,3 +6,27 @@
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+
+	// 验证用户登录功能
+	$.ajax({
+		url: '/api/employee/checkRootLogin',
+		type: 'get',
+		success: function (info) {
+				if(info.error) {
+					location.href = '/admin/login.html?url=' + location.href;
+				}
+		}
+	})
+
+	// 退出登录功能
+	$('.fa-sign-out').parent().on('click', function () {
+		$.ajax({
+			url: '/api/employee/employeeLogout',
+			type: 'get',
+			success: function (info) {
+				if(info.success) {
+					location.href = '/admin/login.html?url=' + location.href;
+				}
+			}
+		})
+	})
